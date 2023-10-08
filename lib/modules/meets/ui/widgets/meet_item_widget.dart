fc89923d151b59cd5a77f49856c7707df5e4adc5
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-08 12:50:18 +0200

    Meet item, Added Profile Avatar and username section

--- lib/modules/meets/ui/widgets/meet_item_widget.dart
+++ lib/modules/meets/ui/widgets/meet_item_widget.dart
@@ -11,27 +11,69 @@ class MeetItem extends StatefulWidget {
 class _MeetItemState extends State<MeetItem> {
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-    MeetStories()
-      ,
-      Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
+    return Stack(
+      children: [
+        MeetStories(),
+        Align(
           alignment: Alignment.bottomRight,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(onPressed: ()=>{}, icon: const Icon(Icons.favorite_outline,size: 32,),color: Colors.white,),
-              const Text("Like",style: TextStyle(color: Colors.white),),
-              IconButton(onPressed: ()=>{}, icon: const Icon(Icons.messenger_outline,size: 32,),color: Colors.white,),
-              const Text("Chat",style: TextStyle(color: Colors.white),),
-              IconButton(onPressed: ()=>{}, icon: const Icon(Icons.info_outline,size: 32,),color: Colors.white,),
-              const Text("Info",style: TextStyle(color: Colors.white),),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.only(right: 8, bottom: 24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(
+                    Icons.favorite_outline,
+                    size: 32,
+                  ),
+                  color: Colors.white,
+                ),
+                const Text(
+                  "Like",
+                  style: TextStyle(color: Colors.white),
+                ),
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(
+                    Icons.messenger_outline,
+                    size: 32,
+                  ),
+                  color: Colors.white,
+                ),
+                const Text(
+                  "Chat",
+                  style: TextStyle(color: Colors.white),
+                ),
+                IconButton(
+                  onPressed: () => {},
+                  icon: const Icon(
+                    Icons.info_outline,
+                    size: 32,
+                  ),
+                  color: Colors.white,
+                ),
+                const Text(
+                  "Info",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
-      )
-    ],);
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 16, right: 8),
+            child: ListTile(
+              leading: CircleAvatar(),
+              title: Text("Username"),
+              subtitle: Text("Location, Age"),
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
