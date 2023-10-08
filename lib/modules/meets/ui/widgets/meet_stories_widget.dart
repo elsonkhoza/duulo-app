1f37c33f62904cec020d84d514234c28cfbbe75d
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-08 12:49:29 +0200

    Meets Stories Update

--- lib/modules/meets/ui/widgets/meet_stories_widget.dart
+++ lib/modules/meets/ui/widgets/meet_stories_widget.dart
@@ -20,48 +20,17 @@ class _MeetStoriesState extends State<MeetStories> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: StoryView(
         storyItems: [
           StoryItem.text(
-            title: "I guess you'd love to see more of our food. That's great.",
+            title: "Image 1",
             backgroundColor: Colors.blue,
           ),
           StoryItem.text(
-            title: "Nice!\n\nTap to continue.",
+            title: "Image 2",
             backgroundColor: Colors.red,
-            textStyle: TextStyle(
-              fontFamily: 'Dancing',
-              fontSize: 40,
-            ),
-          ),
-          StoryItem.pageImage(
-            url:
-            "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fbeautiful%2520woman%2F&psig=AOvVaw07tmLPp773nJW9JpwQhdkC&ust=1696763223800000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIiEtNzl44EDFQAAAAAdAAAAABAE",
-            caption: "Still sampling",
-            controller: storyController,
-          ),
-          StoryItem.pageImage(
-              url: "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif",
-              caption: "Working with gifs",
-              controller: storyController),
-          StoryItem.pageImage(
-            url: "https://media.giphy.com/media/XcA8krYsrEAYXKf4UQ/giphy.gif",
-            caption: "Hello, from the other side",
-            controller: storyController,
-          ),
-          StoryItem.pageImage(
-            url: "https://media.giphy.com/media/XcA8krYsrEAYXKf4UQ/giphy.gif",
-            caption: "Hello, from the other side2",
-            controller: storyController,
           ),
         ],
-        onStoryShow: (s) {
-          print("Showing a story");
-        },
-        onComplete: () {
-          print("Completed a cycle");
-        },
         progressPosition: ProgressPosition.top,
         repeat: false,
         controller: storyController,
